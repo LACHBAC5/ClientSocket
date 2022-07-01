c6d3d72ad8d482d102affc2aeea83f952b7b69e3
lachezar bakalov <labakalov5@gmail.com>
2022-07-01 16:53:53 +0300

    1

--- main.cpp
+++ main.cpp
@@ -2,20 +2,35 @@
 
 #include <iostream>
 
-/*
-Authorization: 
-Digest 
-username="admin", 
-realm="iPolis_00-09-18-52-39-00", 
-nonce="b42f8a4229a68761bdf247d3f03d5618",  
-qop=auth, 
+#include "md5.h"
+
+/* browser get request
+GET /stw-cgi/attributes.cgi/attributes HTTP/1.1
+Host: ivos.ddns.net:321
+User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,;q=0.8
+Accept-Language: en-US,en;q=0.5
+Accept-Encoding: gzip, deflate
+Authorization: Digest username="admin", realm="iPolis_00-09-18-52-39-00", nonce="8c7501ff04be51c53be9477e55e97e17", uri="/stw-cgi/attributes.cgi/attributes", response="bae7ecaf4ee6e15e706d21936b6f5d4f", qop=auth, nc=00000002, cnonce="83ba2d99eed620dd"
+Connection: keep-alive
+Upgrade-Insecure-Requests: 1
 */
 
-int main(){
-    lb::ClientSocket soc("ivos.ddns.net", "321");
+std::string make_digest(std::string username, std::string password, std::string method, std::string realm, std::string nonce, std::string uri, std::string qop, std::string nc, std::string cnonce){
+    std::string response = md5(md5(username+':'+realm+':'+password)+':'+nonce+':'+nc+':'+cnonce+':'+qop+':'+md5(method+':'+uri));
+    return "Digest username=\""+username+"\", realm=\""+realm+"\", nonce=\""+nonce+"\", uri=\""+uri+"\", response=\""+response+"\", qop="+qop+", nc="+nc+", cnonce=\""+cnonce+"\"";
+}
+
+int main(int argc, char *argv[]){
+
+
+    std::string Digest = make_digest(argv[3], argv[4], "GET", "iPolis_00-09-18-52-39-00", "376cb247948cfa008a6ab98abd3d781a", "/stw-cgi/attributes.cgi/attributes", "auth", "00000002", "83ba2d99eed620dd");
+
+    lb::ClientSocket soc(argv[1], argv[2]);
     std::cout << soc.open_connection();
-    std::cout << soc.send_request("GET /stw-cgi/image.cgi?msubmenu=imageenhancements&action=set&Channel=0&Brightness=20 HTTP/1.0\r\nWWW-Authenticate: Digest realm=iPolis_00-09-18-52-39-00 nonce=692172c8112fdc038f7907871631f906 qop=auth\r\n\r\n");
+    std::cout << soc.send_request("GET /stw-cgi/attributes.cgi/attributes HTTP/1.0\r\nHost: ivos.ddns.net:321\r\nWWW-Authenticate: " + Digest + "\r\n\r\n");
 
     soc.fetch_response();
 
+    soc.close_connection();
 }
--- makefile
+++ makefile
@@ -1,13 +1,16 @@
 all: main
 
-main: main.o ClientSocket.o
-	g++ main.o ClientSocket.o -o main
+main: main.o ClientSocket.o md5.o
+	g++ main.o ClientSocket.o md5.o -o main
 
 main.o: main.cpp
 	g++ -c main.cpp
 
 ClientSocket.o: ClientSocket.cpp
 	g++ -c ClientSocket.cpp
+
+md5.o: md5.cpp
+	g++ -c md5.cpp
 
 clean:
 	rm main.o ClientSocket.o
